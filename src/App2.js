adc2e19e5fa00c7c423ce0d1c10bc795eb3f3182
Aritra Basu <aritrabasu71@gmail.com>
2022-08-07 23:36:58 +0530

    update

--- src/App2.js
+++ src/App2.js
@@ -11,7 +11,6 @@ import { BaseOptionChartStyle } from './components/chart/BaseOptionChart';
 export default function App() {
   return (
     <ThemeProvider>
-      hi
       <ScrollToTop />
       <BaseOptionChartStyle />
       <Router />
