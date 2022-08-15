c09812f5066a7c071b54f8ae24a68c5c0a7a6643
Aritra Banerjee <aritra.ban11@gmail.com>
2022-08-15 18:20:10 +0530

    updates

--- src/components/Largegrid.js
+++ src/components/Largegrid.js
@@ -3,13 +3,13 @@ import React from 'react'
 import Iconify from './Iconify'
 
 
-function LargeGrid({title, subheader, count, color, icon, height, width}) {
+function LargeGrid({title, subheader, count, color, icon}) {
   return (
-    <Card sx={{px:2}}>
+    <Card sx={{px:2}}> 
        
         <Grid container>
           <Grid item xs={4} sm={4} md={4} lg={4} xl={4} sx={{borderRight:"1px dashed #ddd", py:2, alignItems:"center", justifyContent:"center", display:"flex"}}>
-            <Iconify icon={icon} width={width || 50} height={height|| 50} color={color} />
+            <Iconify icon={icon} width={50} height={50} />
           </Grid>
           <Grid item xs={8} sm={8} md={8} lg={8} xl={8} sx={{py:2, pl:2}}>
             <Box sx={{display:"flex",alignItems:"center"}}>
